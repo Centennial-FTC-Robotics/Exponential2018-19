e981cee6b1c9d8a667bfc4ed8837d7b57123a9d7
ftf841 <ftf148@gmail.com>
2018-10-07 12:22:11 -0400

    Changed code to use LinearOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExponentialHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExponentialHardware.java
@@ -1,17 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public abstract class ExponentialHardware extends OpMode {
+public abstract class ExponentialHardware extends LinearOpMode {
 
     DcMotor lmotor0;
     DcMotor lmotor1;
     DcMotor rmotor0;
     DcMotor rmotor1;
 
-    public void init() {
-
+    @Override
+    public void runOpMode() throws InterruptedException {
         lmotor0 = hardwareMap.dcMotor.get("lmotor0");
         lmotor1 = hardwareMap.dcMotor.get("lmotor1");
         rmotor0 = hardwareMap.dcMotor.get("rmotor0");
@@ -22,5 +23,9 @@ public abstract class ExponentialHardware extends OpMode {
         lmotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rmotor0.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rmotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lmotor0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        lmotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rmotor0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rmotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
