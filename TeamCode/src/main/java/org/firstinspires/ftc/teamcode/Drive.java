b3cecb709c9928aaffdb30f3986e2a4a79d0592b
Ten086 <85halfnotes@gmail.com>
2018-10-26 15:37:54 -0400

    Added driving and hinge motor methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -16,6 +16,34 @@ public class Drive extends ExponentialMethods {
             lmotor1.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y-Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x,-1,1));
             rmotor0.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y+Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x, -1,1));
             rmotor1.setPower(Range.clip(Math.abs(gamepad1.left_stick_y)*gamepad1.left_stick_y+Math.abs(gamepad1.right_stick_x)*gamepad1.right_stick_x,-1,1));
+
+            int position = hingeMotor.getCurrentPosition();
+            float stickPos = Range.clip(gamepad1.right_stick_y, -1, 1);
+
+            //if at 90 degrees, only move if decreasing angle
+            if (position >= 2240) {
+                if (stickPos < 0) {
+                    hingeMotor.setPower(stickPos);
+                }
+                else {
+                    hingeMotor.setPower(0);
+                }
+            }
+
+            //if at 0 degrees, only move if increasing angle
+            else if (position <= 0){
+                if (stickPos > 0) {
+                    hingeMotor.setPower(stickPos);
+                }
+                else {
+                    hingeMotor.setPower(0);
+                }
+            }
+
+            //if in between 0 and 90 degrees
+            else {
+                hingeMotor.setPower(stickPos);
+            }
             idle();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExponentialMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExponentialMethods.java
@@ -1,7 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
 public abstract class ExponentialMethods extends ExponentialHardware {
 
+    public void runDriveMotors(float leftSpeed, float rightSpeed) {
+        lmotor0.setPower(Range.clip(leftSpeed, -1, 1));
+        lmotor1.setPower(Range.clip(leftSpeed, -1, 1));
+        rmotor0.setPower(Range.clip(rightSpeed, -1, 1));
+        rmotor1.setPower(Range.clip(rightSpeed, -1, 1));
+    }
 
+    public void moveHingeTo(float angle) {
+        hingeMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        angle = Range.clip(angle, 0, 90);
+        int position = (int)(angle * (2240/90));
+        hingeMotor.setTargetPosition(position);
+        hingeMotor.setPower(1);
+        hingeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
 
 }
